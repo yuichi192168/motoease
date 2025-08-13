1de5757cd2b5d05772217397d6ca30236fbebca9
yuichi192168 <yuichi192168@gmail.com>
2025-08-13 23:12:37 +0800

    add some motorycyles and products and accessories and motorcycle parts

--- products/index.php
+++ products/index.php
@@ -9,7 +9,7 @@ $category_filter = isset($_GET['category_filter']) ? explode(",",$_GET['category
             <div class="col-md-4">
                 <h3 class="text-muted">Filters</h3>
                 <hr>
-                <div class="card card-outline shadow card-primary rounded-0">
+                <!-- <div class="card card-outline shadow card-primary rounded-0">
                     <div class="card-header">
                         <h3 class="card-title"><b>Brands</b></h3>
                     </div>
@@ -34,7 +34,7 @@ $category_filter = isset($_GET['category_filter']) ? explode(",",$_GET['category
                             <?php endwhile; ?>
                         </ul>
                     </div>
-                </div>
+                </div> -->
                 <div class="card card-outline shadow card-primary rounded-0">
                     <div class="card-header">
                         <h3 class="card-title"><b>Categories</b></h3>
