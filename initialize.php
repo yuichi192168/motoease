62a4bc5158ef390f1ef278046003243c0bc8ac2e
yuichi192168 <yuichi192168@gmail.com>
2025-10-12 13:08:37 +0800

    configure initialize database

--- initialize.php
+++ initialize.php
@@ -1,11 +1,23 @@
 <?php
+// $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+// if(!defined('base_url')) define('base_url','http://localhost/bpsms/');
+// if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+// if(!defined('dev_data')) define('dev_data',$dev_data);
+// if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+// if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+// if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+// if(!defined('DB_NAME')) define('DB_NAME',"bpsms_db");
+// if(!defined('DB_PORT')) define('DB_PORT',"3307");
+?>
+
+<?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/bpsms/');
+if(!defined('base_url')) define('base_url','https://motoease.ct.ws/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"bpsms_db");
-if(!defined('DB_PORT')) define('DB_PORT',"3307");
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql212.infinityfree.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_40141531");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"192168motoease");
+if(!defined('DB_NAME')) define('DB_NAME',"if0_40141531_motoease");
+if(!defined('DB_PORT')) define('DB_PORT',"3306");
 ?>
