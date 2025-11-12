833317bfe4c8cd08858137bd92673ce3499bab9d
yuichi192168 <yuichi192168@gmail.com>
2025-11-12 22:07:19 +0800

    add the environment and builds deployment configs

---
+++ .render-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+composer install
+php -S 0.0.0.0:10000 -t .
--- initialize.php
+++ initialize.php
@@ -1,13 +1,13 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/bpsms/');
+if(!defined('base_url')) define('base_url','https://motoease.onrender.com/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"if0_40141531_motoease_8");
-if(!defined('DB_PORT')) define('DB_PORT',"3307");
+if(!defined('DB_SERVER')) define('DB_SERVER',"b4ejmxgl0c5c32ejan5s-mysql.services.clever-cloud.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"uczldj9vclddzqrx");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"v4Wt6XD3Molc91cs4szI");
+if(!defined('DB_NAME')) define('DB_NAME',"b4ejmxgl0c5c32ejan5s");
+if(!defined('DB_PORT')) define('DB_PORT',"3306");
 ?>
 
 <?php
