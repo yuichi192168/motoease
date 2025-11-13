1ea3f70b8b210114e5828d46e46ecd9c72e4bdad
yuichi192168 <yuichi192168@gmail.com>
2025-11-13 16:59:33 +0800

    change color blue

--- register.php
+++ register.php
@@ -311,7 +311,7 @@
                   </div>
                   <div class="form-group col-md-6">
                     <label for="email" class="small">Email *</label>
-                    <input type="email" name="email" id="email" class="form-control form-control-sm" placeholder="jsmith@sample.com" required>
+                    <input type="email" name="email" id="email" class="form-control form-control-sm" placeholder="bpsms@gmail.com" required>
                     <div class="error-msg" id="email-error"></div>
                   </div>
                   <div class="form-group col-md-6">
